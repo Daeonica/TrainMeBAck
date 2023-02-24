471d1451b0a5efe31ce8e81aa2fcf350776a4902
Afnan Amin <afnam78@gmail.com>
2023-02-24 22:41:47 +0100

    DOCUMENTACIÓN DE CONSULTAS

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -25,7 +25,18 @@ class CourseController extends AbstractController
      * He cambiado la estructura de las consultas.
      * Cuando queremos buscar un curso, suele ser algo jerárquico.
      * Es decir, tu objetivo es buscar cursos a través de una palabra.
-     * 
+     * La idea es buscar primero en la tabla curso, y luego en tablas que tengan relación con la tabla cursos (usuario, categoría).
+     * ········································
+     * ········································
+     * ····                                ····
+     * ····    ADJUNTO EXPLICACIÓN         ····
+     * ····    PASO POR PASO DE LO         ····
+     * ····    QUE SE HA HECHO PARA        ····
+     * ····    QUE LA FUNCIONALIDAD        ····
+     * ····    DE BÚSQUEDA SEA LA ÓPTIMA   ····
+     * ····                                ····
+     * ········································
+     * ········································
      * -------------------------------------------------------------------------------------------------------------------------------------------------------------
      * -------------------------------------------------------------------------------------------------------------------------------------------------------------
      * PRIMER PASO:                                                                                    -------------------------------------------------------------
