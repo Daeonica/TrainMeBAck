ffb829cd56d99f87e52f6963be63ffc7e23cefbf
Afnan Amin <afnam78@gmail.com>
2023-03-02 15:41:42 +0100

    .

--- migrations/Version20230223171955.php
+++ migrations/Version20230223171955.php
@@ -23,7 +23,6 @@ final class Version20230223171955 extends AbstractMigration
         $this->addSql('CREATE TABLE review (id INT AUTO_INCREMENT NOT NULL, course_id INT NOT NULL, user_id INT NOT NULL, comment VARCHAR(500) DEFAULT NULL, stars INT NOT NULL, review_date DATETIME NOT NULL, INDEX IDX_794381C6591CC992 (course_id), INDEX IDX_794381C6A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C6591CC992 FOREIGN KEY (course_id) REFERENCES course (id)');
         $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C6A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE course RENAME INDEX idx_169e6fb9a76ed300 TO IDX_169E6FB912469DE2');
     }
 
     public function down(Schema $schema): void
@@ -32,6 +31,5 @@ final class Version20230223171955 extends AbstractMigration
         $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C6591CC992');
         $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C6A76ED395');
         $this->addSql('DROP TABLE review');
-        $this->addSql('ALTER TABLE course RENAME INDEX idx_169e6fb912469de2 TO IDX_169E6FB9A76ED300');
     }
 }
