e463267bad88c138e2d85773c6bc78065152fb41
Afnan Amin <afnam78@gmail.com>
2023-05-04 15:41:09 +0200

    .

--- src/Entity/Course.php
+++ src/Entity/Course.php
@@ -184,7 +184,7 @@ class Course
             "document_root" => $this->document_root,
             "price" => $this->price,
             "img_path" => $this->img_path,
-            "video_path" => $this->category->getImgPath(),
+            "video_path" => $this->video_path,
             "user" => $this->user->getDataInArray(),
             "category" => $this->category->getDataInArray(),
             // "buy_user_courses" => $this->buyUserCourses,
